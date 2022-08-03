25d0e01f2615e0f8fb7e463efd6a3a4b86508719
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-08-03 16:56:01 -0500

    comment routes api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const commentRoutes = require('./comment-routes');
 
+router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
