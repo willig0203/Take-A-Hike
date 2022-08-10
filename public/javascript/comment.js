0f48dd0b1ac0c247a37c93b34d28d807a02298fe
Amy McCabe <mcca0168@umn.edu>
2022-08-09 19:15:23 -0500

    things working

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -3,17 +3,17 @@ async function commentFormHandler(event) {
 
      const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
 
-     const post_id1 = window.location.toString().split('/') [
+     const trail_id1 = window.location.toString().split('/') [
         window.location.toString().split('/').length -1
      ];
-     const post_id = post_id1.charAt(0);
-     console.log(comment_text + post_id);
+     const trail_id = trail_id1.charAt(0);
+     console.log(comment_text + trail_id);
 
      if (comment_text) {
         const response = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({
-                post_id,
+                trail_id,
                 comment_text
             }),
             headers: {
@@ -21,6 +21,7 @@ async function commentFormHandler(event) {
             }
         });
         if(response.ok) {
+            console.log(document.location);
             document.location.reload();
         } else {
             alert(response.statusText)
